a82c5629db8732260707a5690a2270d3c7f48a7c
zhouzhongyi1 <zhouzhongyi1@jd.com>
2020-01-17 11:51:08 +0800

    加入feign

--- src/main/java/notes/mvc/web/controller/IndexController.java
+++ src/main/java/notes/mvc/web/controller/IndexController.java
@@ -1,5 +1,7 @@
 package notes.mvc.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import notes.mvc.web.service.FeignTestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +15,26 @@ import org.springframework.web.client.RestTemplate;
  * @ see
  * @since
  */
+@Slf4j
 @RestController
 @RequestMapping("/client")
 public class IndexController {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired(required = false)
+    private FeignTestClient feignTestClient;
 
 
     @RequestMapping("/index")
     public String sayHi() {
         String json = restTemplate.getForObject("http://eureka-provider/provider/index", String.class);
         return json;
+    }
+
+    @RequestMapping("/usefeign")
+    public String useFeign() {
+        log.error("feign测试");
+        return feignTestClient.invokeProvider();
     }
 }
